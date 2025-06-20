b8bd518d9edee2dbdce1b68aaeed1f2b10dcf116
BXNSAVAGE <159540549+BXNSAVAGE@users.noreply.github.com>
2025-06-20 22:26:22 +0200

    Create sendSwapTx.ts

---
+++ sendSwapTx.ts
@@ -0,0 +1,10 @@
+export async function sendSwapTx(
+  wallet: any,
+  connection: any,
+  fromMint: string,
+  toMint: string,
+  amount: number
+) {
+  console.log('Pretending to send swap transaction...');
+  return { success: true };
+}
